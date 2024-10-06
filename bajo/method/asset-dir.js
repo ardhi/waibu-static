fcb0a65f9b0f6d084e6a6f5966378e25120ab571
Ardhi Lukianto <ardhi@lukianto.com>
2024-10-06 21:10:16 +0700

    Updates

--- bajo/method/asset-dir.js
+++ bajo/method/asset-dir.js
@@ -1,7 +1,7 @@
 function assetDir (ns) {
   const dir = this.config.prefix === '' ? '' : `/${this.config.prefix}`
   if (!ns) return dir
-  if (ns === this.app.bajo.mainNs && this.config.mountAppAsRoot) return dir
+  if (ns === this.app.bajo.mainNs && this.config.mountMainAsRoot) return dir
   const plugin = this.app.bajo.getPlugin(ns)
   return dir + '/' + plugin.alias
 }
